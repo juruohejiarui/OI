7507af78c89821061d649d86334f7a46040a2232
何家睿 <dengyiming@dengyimingdeMacBook-Air.local>
2020-09-05 19:33:38 +0800

    b

---
+++ training2020.9.5/T2.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+#include <cmath>
+
+using namespace std;
+
+const int maxn = 3e5 + 5;
+
+int n, id[maxn];
+string str[maxn];
+int main() {
+    freopen("test.in", "r", stdin);
+    freopen("test.out", "w", stdout);
+    
+    return 0;
+}
