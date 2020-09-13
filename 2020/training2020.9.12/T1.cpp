7da3369891ee71ecaf47bdb179bfe0848fbd84d5
何家睿 <dengyiming@dengyimingdeMacBook-Air.local>
2020-09-13 12:56:24 +0800

    update20209.13

---
+++ 2020/training2020.9.12/T1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    // freopen("test.in", "r", stdin);
+    // freopen("test.out", "w", stdout);
+    int n, m; scanf("%d%d", &n, &m);
+    if (m) printf("Yes\n");
+    else printf("No\n");
+    return 0;
+}
